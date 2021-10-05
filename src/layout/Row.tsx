67a878760b559e142cb41f043390dc2c038149d3
David Losert <david.losert@gmx.de>
2021-10-05 12:13:14 +0200

    Episode 01: Removes withGap from Row as it is never used

--- src/layout/Row.tsx
+++ src/layout/Row.tsx
@@ -1,14 +1,9 @@
 import styled from 'styled-components';
 
-interface RowProps {
-  withGap?: boolean;
-}
-
-const Row = styled.div<RowProps>`
+const Row = styled.div`
   display: flex;
   flex-flow: row wrap;
   justify-content: space-between;
-  margin-bottom: ${(props): any => props.withGap ? '10px' : '0px'}
 `;
 
 export {
