dbf178c920fefcc85d511c1fd4992fe6f2ab4ecb
David Losert <david.losert@gmx.de>
2021-10-13 08:15:12 +0200

    Episode 05: Adds react-router and a first basic route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import { createGlobalStyle } from 'styled-components';
 import { PageLayout } from './layout/PageLayout';
 import { PartyOverview } from './app/PartyOverview/PartyOverview';
 import { ReactElement } from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch
+} from 'react-router-dom';
 
 const GlobalStyles = createGlobalStyle`
   body {
@@ -13,9 +18,13 @@ const App = function (): ReactElement {
   return (
     <AppProviders>
       <GlobalStyles />
-      <PageLayout title='Halloween Party Planner'>
-        <PartyOverview />
-      </PageLayout>
+      <Router>
+        <PageLayout title='Halloween Party Planner'>
+          <Switch>
+            <Route exact={ true } path='/' component={ PartyOverview } />
+          </Switch>
+        </PageLayout>
+      </Router>
     </AppProviders>
   );
 };
