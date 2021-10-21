20f8402509ac650e75091be8d52d61c3d944eddf
David Losert <david.losert@gmx.de>
2021-10-21 10:30:17 +0200

    Episode 05: Fixes tests for party-js

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,23 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
 // Jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import '../src/app/i18n/i18nService';
+
+// Allow party-js plugin to work properly in tests
+window.matchMedia = window.matchMedia || function () {
+  return {
+    matches: false,
+    addListener (): void {},
+    removeListener (): void {}
+  };
+};
+
+/* eslint-enable @typescript-eslint/explicit-function-return-type */
+/* eslint-enable @typescript-eslint/no-unnecessary-condition */
+/* eslint-enable @typescript-eslint/no-empty-function */
