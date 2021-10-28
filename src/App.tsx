b698e574689f3f533d520c43031ca6d261e4107c
David Losert <david.losert@gmx.de>
2021-10-28 10:59:45 +0200

    Episode 06: Adds Sentry monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { AppProviders } from './AppProviders';
+import { createBrowserHistory } from 'history';
 import { createGlobalStyle } from 'styled-components';
+import { Integrations } from '@sentry/tracing';
 import { PageLayout } from './layout/PageLayout';
 import { PartyEditPage } from './app/PartyEdit/PartyEditPage';
 import { PartyOverview } from './app/PartyOverview/PartyOverview';
@@ -7,9 +9,10 @@ import { ReactElement } from 'react';
 import { routes } from './app/routes';
 import {
   Route,
-  BrowserRouter as Router,
+  Router,
   Switch
 } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
 
 const GlobalStyles = createGlobalStyle`
   body {
@@ -17,20 +20,53 @@ const GlobalStyles = createGlobalStyle`
   };
 `;
 
+/* eslint-disable no-process-env */
+/* eslint-disable @typescript-eslint/naming-convention */
+const { NODE_ENV, REACT_APP_SENTRY_DSN } = process.env;
+const useSentry: boolean = NODE_ENV === 'production' && Boolean(REACT_APP_SENTRY_DSN);
+const history = createBrowserHistory();
+/* eslint-enable @typescript-eslint/naming-convention */
+/* eslint-enable no-process-env */
+
+Sentry.init({
+  enabled: useSentry,
+  dsn: REACT_APP_SENTRY_DSN,
+  integrations: [ new Integrations.BrowserTracing({
+    tracingOrigins: [ 'localhost', /^\//u ],
+    routingInstrumentation: Sentry.reactRouterV5Instrumentation(history)
+  }) ],
+  release: 'reactober@1.0.0',
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1
+});
+
+const SentryRoute = Sentry.withSentryRouting(Route);
+
 const App = function (): ReactElement {
   return (
     <AppProviders>
       <GlobalStyles />
-      <Router>
+      <Router history={ history }>
         <PageLayout title='Halloween Party Planner'>
           <Switch>
-            <Route exact={ true } path='/' component={ PartyOverview } />
-            <Route path={ routes.editParty.routerUrl } component={ PartyEditPage } />
+            <SentryRoute exact={ true } path='/' component={ PartyOverview } />
+            <SentryRoute path={ routes.editParty.routerUrl } component={ PartyEditPage } />
           </Switch>
         </PageLayout>
       </Router>
+      <button
+        onClick={ (): void => {
+          throw new Error('This appears in Sentry!');
+        } }
+      >Send Error to Sentry and crash the App!
+      </button>
     </AppProviders>
   );
 };
 
-export { App };
+const ProfiledApp = Sentry.withProfiler(App);
+
+export { ProfiledApp };
